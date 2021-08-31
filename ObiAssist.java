2c8cfd15646b81513858c0904f0926a8a77e97d0
ProfKambing <80607981+ProfKambing@users.noreply.github.com>
2021-08-31 15:34:33 +0800

    improvements

--- ObiAssist.java
+++ ObiAssist.java
@@ -5,23 +5,13 @@ import me.vaimok.skid.client.modules.Module;
 import me.vaimok.skid.client.setting.Setting;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockObsidian;
-import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.network.play.client.CPacketHeldItemChange;
-import net.minecraft.potion.Potion;
-import net.minecraft.util.CombatRules;
-import net.minecraft.util.DamageSource;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.world.Explosion;
-import net.minecraft.world.World;
+
 
 /**
  @author kambing
@@ -34,17 +24,18 @@ public final class ObiAssist extends Module {
     private final Setting<Boolean> packet = register(new Setting<Boolean>("PacketSwitch", true));
     private final Setting<Boolean> packethand = register(new Setting<Boolean>("PacketHand", true));
     private final Setting<Boolean> render = register(new Setting<Boolean>("Render", true));
+    private final Setting<Double> range = register(new Setting<Double>("TargetMaxRange", 10.0, 5.0, 15.0));
+    private final Setting<Double> delay = register(new Setting<Double>("Delay (MS)", 200.0, 0.0, 500.0));
+
 
     private static ObiAssist instance;
 
+    private final Timer delayTimer = new Timer ( );
+
     public ObiAssist() {
-        super("ObiAssist", "place obsidian to support your autocrystal", Category.COMBAT, true, false, false);
+        super("ObiAssist", "place obsidian to support your  .tocrystal", Category.COMBAT, true, false, false);
         instance = this;
     }
-
-    /*
-     * this is kinda chinese
-     */
 
     @Override
     public void onUpdate() {
@@ -59,67 +50,74 @@ public final class ObiAssist extends Module {
         if (NekoAura.getInstance().placePos != null)
             return;
 
-        //switch shit kinda messy
-        if (NekoAura.getInstance().isEnabled() && NekoAura.getInstance().currentTarget != null) {
+        if (NekoAura.getInstance().isEnabled() && NekoAura.getInstance().currentTarget != null && NekoAura.getInstance().placePos == null) {
             if (slot != -1) {
-                if (mc.player.inventory.currentItem != slot) {
-                    if (packet.getValue()) {
-                        if (mc.player.isHandActive()) {
-                            hand = mc.player.getActiveHand();
+                if (delayTimer.passedMs(delay.getValue().longValue())) {
+                    if (mc.player.inventory.currentItem != slot) {
+                        if (packet.getValue()) {
+                            if (mc.player.isHandActive()) {
+                                hand = mc.player.getActiveHand();
+                            }
+                            mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
                         }
-                        mc.player.connection.sendPacket(new CPacketHeldItemChange(slot));
                     }
                 }
             }
         }
         try {
 
-            float range = NekoAura.getInstance().placeRange.getValue();
+            if (!(target.getDistance(mc.player) > range.getValue())) {
+                float range = NekoAura.getInstance().placeRange.getValue();
 
-            float targetDMG = 0.0f;
-            float minDmg = NekoAura.getInstance().minMinDmg.getValue();
+                float targetDMG = 0.0f;
+                float minDmg = NekoAura.getInstance().minMinDmg.getValue();
 
-            BlockPos targetPos = null;
+                BlockPos targetPos = null;
 
-            for (BlockPos pos : BlockUtil.getSphere(PlayerUtil.getPlayerPosFloored(), range, (int) range, false, true, 0)) {
-                BlockUtil.ValidResult result = BlockUtil.valid(pos);
+                for (BlockPos pos : BlockUtil.getSphere(PlayerUtil.getPlayerPosFloored(), range, (int) range, false, true, 0)) {
+                    BlockUtil.ValidResult result = BlockUtil.valid(pos);
 
-                if (result != BlockUtil.ValidResult.Ok)
-                    continue;
+                    if (result != BlockUtil.ValidResult.Ok)
+                        continue;
 
-                if (!CanPlaceCrystalIfObbyWasAtPos(pos))
-                    continue;
+                    if (!crystalPlacePosPredict(pos))
+                        continue;
 
-                float tempDMG = DamageUtil.calculateDamage(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, target);
+                    float tempDMG = DamageUtil.calculateDamage(pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, target);
 
 
-                if (tempDMG < minDmg)
-                    continue;
+                    if (tempDMG < minDmg)
+                        continue;
 
-                if (tempDMG >= targetDMG) {
-                    targetPos = pos;
-                    targetDMG = tempDMG;
+                    if (tempDMG >= targetDMG) {
+                        targetPos = pos;
+                        targetDMG = tempDMG;
+                    }
                 }
-            }
 
-            if (targetPos != null) {
-                BlockUtil.placeBlock(targetPos);
-            }
-            if (packet.getValue()) {
-                if (slot != -1) {
-                    mc.player.connection.sendPacket(new CPacketHeldItemChange(old));
-                    if (packethand.getValue() && hand != null) {
-                        mc.player.setActiveHand(hand);
+                if (targetPos != null && render.getValue()) {
+                    NekoAura.getInstance().renderPos = targetPos;
+                }
+
+                if (targetPos != null) {
+                    BlockUtil.placeBlock(targetPos);
+                }
+                if (packet.getValue()) {
+                    if (slot != -1) {
+                        mc.player.connection.sendPacket(new CPacketHeldItemChange(old));
+                        if (packethand.getValue() && hand != null) {
+                            mc.player.setActiveHand(hand);
+                        }
                     }
                 }
             }
 
-        }catch (NullPointerException ignored) {
+        } catch (NullPointerException ignored) {
             //to avoid ticking entity crash
         }
     }
 
-    public static boolean CanPlaceCrystalIfObbyWasAtPos(final BlockPos pos) {
+    public static boolean crystalPlacePosPredict(final BlockPos pos) {
 
         final Block floor = mc.world.getBlockState(pos.add(0, 1, 0)).getBlock();
         final Block ceil = mc.world.getBlockState(pos.add(0, 2, 0)).getBlock();
